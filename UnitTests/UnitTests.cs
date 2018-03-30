348aaef3d43e4ecd5f0c33c2c206dc67b83f5c41
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-03-30 22:25:46 +0900

    add test data.

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -46,6 +46,10 @@ namespace ReplaceGuid
     {
         const string input1 = @"[Guid(""00000000-0000-0000-0000-000000000000"")]";
         const string result1 = @"[Guid(""01020304-0506-0708-090a-0b0c0d0e0f10"")]";
+        const string input2 = @"""00000000-0000-0000-0000-000000000000""";
+        const string result2 = @"""01020304-0506-0708-090a-0b0c0d0e0f10""";
+        const string input3 = @"{00000000-0000-0000-0000-000000000000}";
+        const string result3 = @"{01020304-0506-0708-090a-0b0c0d0e0f10}";
         private GuidGenerater guidGenerator;
 
         [SetUp]
@@ -61,11 +65,17 @@ namespace ReplaceGuid
         }
 
         [Test]
-        public void TestGuidWithBraces()
+        public void TestGuidWithBrackets()
         {
             var replaceWithGuid = new ReplaceWithNewGuid(this.guidGenerator.NewGuid);
             var output = replaceWithGuid.ReplaceSameGuidToSameGuid(input1);
             Assert.That(output, Is.EqualTo(result1));
+
+            var output2 = replaceWithGuid.ReplaceSameGuidToSameGuid(input2);
+            Assert.That(output2, Is.EqualTo(result2));
+
+            var output3 = replaceWithGuid.ReplaceSameGuidToSameGuid(input3);
+            Assert.That(output3, Is.EqualTo(result3));
         }
     }
 }
