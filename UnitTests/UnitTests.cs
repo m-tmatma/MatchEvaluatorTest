7f658581fbef3991c2e2bc66518239ef0de047fa
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-04-01 11:36:40 +0900

    add xml comments.

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -9,29 +9,68 @@ namespace ReplaceGuid
     using System.Text;
     using NUnit.Framework;
 
+    /// <summary>
+    /// unit test for ReplaceWithNewGuid
+    /// </summary>
     [TestFixture]
     public class UnitTests
     {
+        /// <summary>
+        /// input data for test1
+        /// </summary>
         const string input1 = @"[Guid(""00000000-0000-0000-0000-000000000000"")]";
+
+        /// <summary>
+        /// expected output data for test1
+        /// </summary>
         const string result1 = @"[Guid(""01020304-0506-0708-090a-0b0c0d0e0f10"")]";
+
+        /// <summary>
+        /// input data for test2
+        /// </summary>
         const string input2 = @"""00000000-0000-0000-0000-000000000000""";
+
+        /// <summary>
+        /// expected output data for test2
+        /// </summary>
         const string result2 = @"""01020304-0506-0708-090a-0b0c0d0e0f10""";
+
+        /// <summary>
+        /// input data for test3
+        /// </summary>
         const string input3 = @"{00000000-0000-0000-0000-000000000000}";
+
+        /// <summary>
+        /// expected output data for test3
+        /// </summary>
         const string result3 = @"{01020304-0506-0708-090a-0b0c0d0e0f10}";
+
+        /// <summary>
+        /// GUID generator class to make unit-testing easier
+        /// </summary>
         private GuidGenerater guidGenerator;
 
+        /// <summary>
+        /// setup method to be called to initialize unit tests
+        /// </summary>
         [SetUp]
         public void SetUp()
         {
             this.guidGenerator = new GuidGenerater();
         }
 
+        /// <summary>
+        /// cleanup method to be called to cleanup unit tests
+        /// </summary>
         [TearDown]
         public void TearDown()
         {
             this.guidGenerator = null;
         }
 
+        /// <summary>
+        /// unit test method
+        /// </summary>
         [Test]
         public void TestGuidWithBrackets()
         {
