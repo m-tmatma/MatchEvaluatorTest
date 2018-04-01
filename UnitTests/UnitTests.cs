128be0eadecbc13884f4ac37bb0f61253c7ff97e
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-04-01 17:07:15 +0900

    add 'Randomly concatenated pattern test'

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -86,6 +86,34 @@ namespace ReplaceGuid
         private const string ResultGUIDStruct = @"static const GUID <<name>> = {0x01020304,0x0506,0x0708,{0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10}};";
 
         /// <summary>
+        /// Input strings
+        /// </summary>
+        private static readonly string[] Inputs = new string[]
+        {
+            Input1,
+            Input2,
+            Input3,
+            Input4,
+            InputOLECREATE ,
+            InputDefineGuid,
+            InputGUIDStruct,
+        };
+
+        /// <summary>
+        /// Result strings
+        /// </summary>
+        private static readonly string[] Results = new string[]
+        {
+            Result1,
+            Result2,
+            Result3,
+            Result4,
+            ResultOLECREATE,
+            ResultDefineGuid,
+            ResultGUIDStruct,
+        };
+
+        /// <summary>
         /// GUID generator class to make unit-testing easier
         /// </summary>
         private GuidGenerater guidGenerator;
@@ -128,6 +156,64 @@ namespace ReplaceGuid
             Console.WriteLine("input   : " + input);
             Console.WriteLine("output  : " + output);
             Console.WriteLine("expected: " + expected);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        /// <summary>
+        /// Randomly concatenated pattern test
+        /// </summary>
+        /// <param name="count">loop count</param>
+        /// <see href="https://github.com/nunit/docs/wiki/TestCase-Attribute">TestCase Attribute</see>
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(7)]
+        [TestCase(10)]
+        [TestCase(20)]
+        [TestCase(50)]
+        [TestCase(100)]
+        public void TestGuidByRandomlyConcatenatedPattern(int count)
+        {
+            // check data size
+            Assert.That(Inputs.Length, Is.GreaterThan(0));
+            Assert.That(Results.Length, Is.GreaterThan(0));
+            Assert.That(Inputs.Length, Is.EqualTo(Results.Length));
+
+            var builderInput = new StringBuilder();
+            var builderResult = new StringBuilder();
+            var random = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                int index = random.Next(0, Inputs.Length - 1);
+                var separator = string.Format("------ {0} ------", i);
+                builderInput.Append(separator);
+                builderInput.Append(Environment.NewLine);
+                builderInput.Append(Inputs[index]);
+                builderInput.Append(Environment.NewLine);
+                builderInput.Append(Environment.NewLine);
+
+                builderResult.Append(separator);
+                builderResult.Append(Environment.NewLine);
+                builderResult.Append(Results[index]);
+                builderResult.Append(Environment.NewLine);
+                builderResult.Append(Environment.NewLine);
+            }
+
+            var input = builderInput.ToString();
+            var output = builderResult.ToString();
+
+            var replaceWithGuid = new ReplaceWithNewGuid(this.guidGenerator.NewGuid);
+            var expected = replaceWithGuid.ReplaceSameGuidToSameGuid(input);
+
+            Console.WriteLine("input: ");
+            Console.WriteLine(input);
+
+            Console.WriteLine("output: ");
+            Console.WriteLine(output);
+
+            Console.WriteLine("expected: ");
+            Console.WriteLine(expected);
+
             Assert.That(output, Is.EqualTo(expected));
         }
     }
