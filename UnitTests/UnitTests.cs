e56f1b690eb78b7ff75daa8675d75aaf847bd4fc
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-04-01 15:33:33 +0900

    use TestCase

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -76,6 +76,26 @@ namespace ReplaceGuid
         private const string ResultGUIDStruct = @"static const GUID <<name>> = {0x01020304,0x0506,0x0708,{0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10}};";
 
         /// <summary>
+        /// input data for combination
+        /// </summary>
+        private const string InputConcatenated = Input1 + 
+                                                 Input2 +
+                                                 Input3 +
+                                                 InputOLECREATE +
+                                                 InputDefineGuid +
+                                                 InputGUIDStruct;
+
+        /// <summary>
+        /// expected output data for combination
+        /// </summary>
+        private const string ResultConcatenated = Result1 + 
+                                                  Result2 +
+                                                  Result3 +
+                                                  ResultOLECREATE +
+                                                  ResultDefineGuid +
+                                                  ResultGUIDStruct;
+
+        /// <summary>
         /// GUID generator class to make unit-testing easier
         /// </summary>
         private GuidGenerater guidGenerator;
@@ -101,45 +121,24 @@ namespace ReplaceGuid
         /// <summary>
         /// unit test method
         /// </summary>
-        [Test]
-        public void TestGuidWithBrackets()
+        /// <see href="https://github.com/nunit/docs/wiki/TestCase-Attribute">TestCase Attribute</see>
+        /// <see href="https://github.com/nunit/docs/wiki/TestCaseSource-Attribute">TestCaseSource Attribute</see>
+        /// <see href="https://github.com/nunit/docs/wiki/TestCaseData">TestCaseData</see>
+        [TestCase(Input1, Result1)]
+        [TestCase(Input2, Result2)]
+        [TestCase(Input3, Result3)]
+        [TestCase(InputOLECREATE, ResultOLECREATE)]
+        [TestCase(InputDefineGuid, ResultDefineGuid)]
+        [TestCase(InputGUIDStruct, ResultGUIDStruct)]
+        [TestCase(InputConcatenated, ResultConcatenated)]
+        public void TestGuidByStaticPattern(string input, string expected)
         {
             var replaceWithGuid = new ReplaceWithNewGuid(this.guidGenerator.NewGuid);
-            var output1 = replaceWithGuid.ReplaceSameGuidToSameGuid(Input1);
-            Console.WriteLine("input   : " + Input1);
-            Console.WriteLine("output  : " + output1);
-            Console.WriteLine("expected: " + Result1);
-            Assert.That(output1, Is.EqualTo(Result1));
-
-            var output2 = replaceWithGuid.ReplaceSameGuidToSameGuid(Input2);
-            Console.WriteLine("input   : " + Input2);
-            Console.WriteLine("output  : " + output2);
-            Console.WriteLine("expected: " + Result2);
-            Assert.That(output2, Is.EqualTo(Result2));
-
-            var output3 = replaceWithGuid.ReplaceSameGuidToSameGuid(Input3);
-            Console.WriteLine("input   : " + Input3);
-            Console.WriteLine("output  : " + output3);
-            Console.WriteLine("expected: " + Result3);
-            Assert.That(output3, Is.EqualTo(Result3));
-
-            var outputOLECreate = replaceWithGuid.ReplaceSameGuidToSameGuid(InputOLECREATE);
-            Console.WriteLine("input   : " + InputOLECREATE);
-            Console.WriteLine("output  : " + outputOLECreate);
-            Console.WriteLine("expected: " + ResultOLECREATE);
-            Assert.That(outputOLECreate, Is.EqualTo(ResultOLECREATE));
-
-            var outputDefineGuid = replaceWithGuid.ReplaceSameGuidToSameGuid(InputDefineGuid);
-            Console.WriteLine("input   : " + InputDefineGuid);
-            Console.WriteLine("output  : " + outputDefineGuid);
-            Console.WriteLine("expected: " + ResultDefineGuid);
-            Assert.That(outputDefineGuid, Is.EqualTo(ResultDefineGuid));
-
-            var outputGUIDStruct = replaceWithGuid.ReplaceSameGuidToSameGuid(InputGUIDStruct);
-            Console.WriteLine("input   : " + InputGUIDStruct);
-            Console.WriteLine("output  : " + outputGUIDStruct);
-            Console.WriteLine("expected: " + ResultGUIDStruct);
-            Assert.That(outputGUIDStruct, Is.EqualTo(ResultGUIDStruct));
+            var output = replaceWithGuid.ReplaceSameGuidToSameGuid(input);
+            Console.WriteLine("input   : " + input);
+            Console.WriteLine("output  : " + output);
+            Console.WriteLine("expected: " + expected);
+            Assert.That(output, Is.EqualTo(expected));
         }
     }
 }
