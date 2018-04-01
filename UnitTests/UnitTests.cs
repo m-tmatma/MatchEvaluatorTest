32925a4461056d3db80e767ba2cea82826c7440a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-04-01 12:39:05 +0900

    add unit tests.

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -46,6 +46,36 @@ namespace ReplaceGuid
         private const string Result3 = @"{01020304-0506-0708-090a-0b0c0d0e0f10}";
 
         /// <summary>
+        /// input data for IMPLEMENT_OLECREATE
+        /// </summary>
+        private const string InputOLECREATE = @"IMPLEMENT_OLECREATE(<<class>>, <<external_name>>, 0x00000000, 0x0000, 0x0000, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);";
+
+        /// <summary>
+        /// expected output data for IMPLEMENT_OLECREATE
+        /// </summary>
+        private const string ResultOLECREATE = @"IMPLEMENT_OLECREATE(<<class>>, <<external_name>>, 0x01020304, 0x0506, 0x0708, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10);";
+
+        /// <summary>
+        /// input data for DEFINE_GUID
+        /// </summary>
+        private const string InputDEFINE_GUID = @"DEFINE_GUID(<<name>>, 0x00000000, 0x0000, 0x0000, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);";
+
+        /// <summary>
+        /// expected output data for DEFINE_GUID
+        /// </summary>
+        private const string ResultDEFINE_GUID = @"DEFINE_GUID(<<name>>, 0x01020304, 0x0506, 0x0708, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10);";
+
+        /// <summary>
+        /// input data for static const GUID
+        /// </summary>
+        private const string InputGUIDStruct = @"static const GUID <<name>> = {0x00000000,0x0000,0x0000,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}};";
+
+        /// <summary>
+        /// expected output data for static const GUID
+        /// </summary>
+        private const string ResultGUIDStruct = @"static const GUID <<name>> = {0x01020304,0x0506,0x0708,{0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10}};";
+
+        /// <summary>
         /// GUID generator class to make unit-testing easier
         /// </summary>
         private GuidGenerater guidGenerator;
@@ -75,14 +105,41 @@ namespace ReplaceGuid
         public void TestGuidWithBrackets()
         {
             var replaceWithGuid = new ReplaceWithNewGuid(this.guidGenerator.NewGuid);
-            var output = replaceWithGuid.ReplaceSameGuidToSameGuid(Input1);
-            Assert.That(output, Is.EqualTo(Result1));
+            var output1 = replaceWithGuid.ReplaceSameGuidToSameGuid(Input1);
+            Console.WriteLine("input   : " + Input1);
+            Console.WriteLine("output  : " + output1);
+            Console.WriteLine("expected: " + Result1);
+            Assert.That(output1, Is.EqualTo(Result1));
 
             var output2 = replaceWithGuid.ReplaceSameGuidToSameGuid(Input2);
+            Console.WriteLine("input   : " + Input2);
+            Console.WriteLine("output  : " + output2);
+            Console.WriteLine("expected: " + Result2);
             Assert.That(output2, Is.EqualTo(Result2));
 
             var output3 = replaceWithGuid.ReplaceSameGuidToSameGuid(Input3);
+            Console.WriteLine("input   : " + Input3);
+            Console.WriteLine("output  : " + output3);
+            Console.WriteLine("expected: " + Result3);
             Assert.That(output3, Is.EqualTo(Result3));
+
+            var outputOLECreate = replaceWithGuid.ReplaceSameGuidToSameGuid(InputOLECREATE);
+            Console.WriteLine("input   : " + InputOLECREATE);
+            Console.WriteLine("output  : " + outputOLECreate);
+            Console.WriteLine("expected: " + ResultOLECREATE);
+            Assert.That(outputOLECreate, Is.EqualTo(ResultOLECREATE));
+
+            var outputDEFINE_GUID = replaceWithGuid.ReplaceSameGuidToSameGuid(InputDEFINE_GUID);
+            Console.WriteLine("input   : " + InputDEFINE_GUID);
+            Console.WriteLine("output  : " + outputDEFINE_GUID);
+            Console.WriteLine("expected: " + ResultDEFINE_GUID);
+            Assert.That(outputDEFINE_GUID, Is.EqualTo(ResultDEFINE_GUID));
+
+            var outputGUIDStruct = replaceWithGuid.ReplaceSameGuidToSameGuid(InputGUIDStruct);
+            Console.WriteLine("input   : " + InputGUIDStruct);
+            Console.WriteLine("output  : " + outputGUIDStruct);
+            Console.WriteLine("expected: " + ResultGUIDStruct);
+            Assert.That(outputGUIDStruct, Is.EqualTo(ResultGUIDStruct));
         }
     }
 }
