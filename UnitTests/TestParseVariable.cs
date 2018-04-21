12c10795374129382e0d37f0239a27b7a3200d45
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-04-22 07:51:52 +0900

    add test for variable with index.

--- UnitTests/TestParseVariable.cs
+++ UnitTests/TestParseVariable.cs
@@ -41,5 +41,24 @@ namespace Unittest
             Console.WriteLine("expected: " + expected);
             Assert.That(output, Is.EqualTo(expected));
         }
+
+        [TestCase("Variable1", "VariableA", 1)]
+        [TestCase("Variable2", "VariableB", 2)]
+        [TestCase("Variable3", "VariableC", 3)]
+        [TestCase("Variable4", "VariableD", 4)]
+        public void Test_ParseVariableIndex(string inputKeyword, string outputKeyword, int index)
+        {
+            var translationTable = new Dictionary<string, string>();
+            translationTable[inputKeyword] = outputKeyword;
+
+            var indexStr = "(" + index.ToString() + ")";
+            var input = "{" + inputKeyword + indexStr + "}";
+            var expected = "{" + outputKeyword + indexStr + "}";
+            var output = ProcessVariable.ReplaceVariable(input, translationTable);
+            Console.WriteLine("input   : " + input);
+            Console.WriteLine("output  : " + output);
+            Console.WriteLine("expected: " + expected);
+            Assert.That(output, Is.EqualTo(expected));
+        }
     }
 }
