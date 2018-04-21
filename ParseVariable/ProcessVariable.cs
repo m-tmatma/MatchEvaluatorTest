37388e217cacadfb4dd25085128ca4c6fb7f418e
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-04-21 22:11:28 +0900

    implement replace keywords in variables and tests.

---
+++ ParseVariable/ProcessVariable.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ParseVariable
+{
+    public class ProcessVariable
+    {
+        const string regexStrLeftSep = @"(?<={)";
+        const string regexStrkeyword = @"(?<keyword>\w+)";
+        const string regexStrIndex = @"(?<index>\d+)";
+        const string regexStrRightSep = @"(?=})";
+        const string regexStr = regexStrLeftSep + regexStrkeyword + @"(" + @"\(" + regexStrIndex + @"\)" + @")?" + regexStrRightSep;
+        static private Regex reg = new Regex(regexStr);
+
+        /// <summary>
+        /// class to provide a delegate of MatchEvaluator
+        /// </summary>
+        internal class MatchEvaluatorHandler
+        {
+            private Dictionary<string, string> m_translationTable;
+
+            public MatchEvaluatorHandler(Dictionary<string, string> translationTable)
+            {
+                this.m_translationTable = translationTable;
+            }
+
+            /// <summary>
+            /// delegate for ReplaceVariable
+            /// </summary>
+            /// <param name="m"></param>
+            /// <returns></returns>
+            public string delegateReplace(Match m)
+            {
+                if (!m.Groups["keyword"].Success)
+                {
+                    return m.Groups[0].Value;
+                }
+
+                // get matched keyword
+                var keyword = m.Groups["keyword"].Value;
+
+                // replace a keyword with newer one.
+                if (this.m_translationTable.ContainsKey(keyword))
+                {
+                    keyword = this.m_translationTable[keyword];
+                }
+
+                if (m.Groups["index"].Success)
+                {
+                    // with index
+                    var index = m.Groups["index"].Value;
+                    return keyword + "(" + index + ")";
+                }
+                else
+                {
+                    // without index
+                    return keyword;
+                }
+            }
+        }
+
+        /// <summary>
+        /// public interface to replace keyword
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="translationTable"></param>
+        /// <returns></returns>
+        public static string ReplaceVariable(string input, Dictionary<string, string> translationTable)
+        {
+            var evaluateHander = new MatchEvaluatorHandler(translationTable);
+            var myEvaluator = new MatchEvaluator(evaluateHander.delegateReplace);
+
+            // Replace matched characters using the delegate method.
+            var output = reg.Replace(input, myEvaluator);
+            return output;
+        }
+    }
+}
---
+++ UnitTests/TestParseVariable.cs
@@ -0,0 +1,45 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestParseVariable.cs" company="Masaru Tsuchiyama">
+//     Copyright (c) Masaru Tsuchiyama. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Unittest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using NUnit.Framework;
+    using ParseVariable;
+
+    [TestFixture]
+    public class TestParseVariable
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [TestCase("Variable1", "VariableA")]
+        [TestCase("Variable2", "VariableB")]
+        [TestCase("Variable3", "VariableC")]
+        [TestCase("Variable4", "VariableD")]
+        public void Test_ParseVariable(string inputKeyword, string outputKeyword)
+        {
+            var translationTable = new Dictionary<string, string>();
+            translationTable[inputKeyword] = outputKeyword;
+
+            var input = "{" + inputKeyword + "}";
+            var expected = "{" + outputKeyword + "}";
+            var output = ProcessVariable.ReplaceVariable(input, translationTable);
+            Console.WriteLine("input   : " + input);
+            Console.WriteLine("output  : " + output);
+            Console.WriteLine("expected: " + expected);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+    }
+}
