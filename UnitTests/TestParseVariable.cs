cfdf2e04105571f20cd0bcd6537e8a76a2702555
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-04-22 08:06:34 +0900

    add test for invalid brackets.

--- UnitTests/TestParseVariable.cs
+++ UnitTests/TestParseVariable.cs
@@ -60,5 +60,30 @@ namespace Unittest
             Console.WriteLine("expected: " + expected);
             Assert.That(output, Is.EqualTo(expected));
         }
+
+
+        [TestCase("Variable1", "VariableA", 1, "[", "]")]
+        [TestCase("Variable2", "VariableB", 2, "{", "}")]
+        [TestCase("Variable3", "VariableC", 3, "<", ">")]
+        [TestCase("Variable4", "VariableD", 4, "[", "")]
+        [TestCase("Variable5", "VariableE", 5, "{", "")]
+        [TestCase("Variable6", "VariableF", 6, "(", "")]
+        [TestCase("Variable7", "VariableG", 7, "", "]")]
+        [TestCase("Variable8", "VariableH", 8, "", "}")]
+        [TestCase("Variable9", "VariableI", 9, "", ")")]
+        public void Test_ParseVariableINvalidIndex(string inputKeyword, string outputKeyword, int index, string left, string right)
+        {
+            var translationTable = new Dictionary<string, string>();
+            translationTable[inputKeyword] = outputKeyword;
+
+            var indexStr = left + index.ToString() + right;
+            var input = "{" + inputKeyword + indexStr + "}";
+            var expected = input;
+            var output = ProcessVariable.ReplaceVariable(input, translationTable);
+            Console.WriteLine("input   : " + input);
+            Console.WriteLine("output  : " + output);
+            Console.WriteLine("expected: " + expected);
+            Assert.That(output, Is.EqualTo(expected));
+        }
     }
 }
