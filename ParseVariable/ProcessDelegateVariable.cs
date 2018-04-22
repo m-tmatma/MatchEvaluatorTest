c020a0628b73b7958c098eae662d6346d2f33c5a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-04-22 09:51:21 +0900

    add keyword substitution by delegate.

---
+++ ParseVariable/ProcessDelegateVariable.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ParseVariable
+{
+    public class ProcessDelegateVariable
+    {
+        const string regexStrLeftSep = @"(?<={)";
+        const string regexStrkeyword = @"(?<keyword>\w+)";
+        const string regexStrIndex = @"(?<index>\d+)";
+        const string regexStrRightSep = @"(?=})";
+        const string regexStr = regexStrLeftSep + regexStrkeyword + @"(" + @"\(" + regexStrIndex + @"\)" + @")?" + regexStrRightSep;
+        static private Regex reg = new Regex(regexStr);
+
+        public delegate string GetNewText(string keyword, int index);
+
+        /// <summary>
+        /// class to provide a delegate of MatchEvaluator
+        /// </summary>
+        internal class MatchEvaluatorHandler
+        {
+            private Dictionary<string, GetNewText> m_translationTable;
+
+            public MatchEvaluatorHandler(Dictionary<string, GetNewText> translationTable)
+            {
+                this.m_translationTable = translationTable;
+            }
+
+            /// <summary>
+            /// delegate for ReplaceVariable
+            /// </summary>
+            /// <param name="m"></param>
+            /// <returns></returns>
+            public string delegateReplace(Match m)
+            {
+                if (!m.Groups["keyword"].Success)
+                {
+                    return m.Groups[0].Value;
+                }
+
+                // get matched keyword
+                var keyword = m.Groups["keyword"].Value;
+                var outData = keyword;
+                var index = -1;
+
+                if (m.Groups["index"].Success)
+                {
+                    // with index
+                    index = int.Parse(m.Groups["index"].Value);
+                }
+
+                if (this.m_translationTable.ContainsKey(keyword))
+                {
+                    outData = this.m_translationTable[keyword](keyword, index);
+                }
+                return outData;
+            }
+        }
+
+        /// <summary>
+        /// public interface to replace keyword
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="translationTable"></param>
+        /// <returns></returns>
+        public static string ReplaceVariable(string input, Dictionary<string, GetNewText> translationTable)
+        {
+            var evaluateHander = new MatchEvaluatorHandler(translationTable);
+            var myEvaluator = new MatchEvaluator(evaluateHander.delegateReplace);
+
+            // Replace matched characters using the delegate method.
+            var output = reg.Replace(input, myEvaluator);
+            return output;
+        }
+    }
+}
---
+++ UnitTests/TestProcessDelegateVariable.cs
@@ -0,0 +1,161 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestProcessDelegateVariable.cs" company="Masaru Tsuchiyama">
+//     Copyright (c) Masaru Tsuchiyama. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Unittest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using NUnit.Framework;
+    using ParseVariable;
+
+    [TestFixture]
+    public class TestProcessDelegateVariable
+    {
+        private static Dictionary<string, string> keywordMap = new Dictionary<string, string>()
+        {
+            { "Variable1", "VariableA" },
+            { "Variable2", "VariableB" },
+            { "Variable3", "VariableC" },
+            { "Variable4", "VariableD" },
+            { "Variable5", "VariableE" },
+            { "Variable6", "VariableF" },
+            { "Variable7", "VariableG" },
+            { "Variable8", "VariableH" },
+            { "Variable9", "VariableI" },
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [TestCase("Variable1", "VariableA")]
+        [TestCase("Variable2", "VariableB")]
+        [TestCase("Variable3", "VariableC")]
+        [TestCase("Variable4", "VariableD")]
+        public void Test_ParseVariable(string inputKeyword, string outputKeyword)
+        {
+            var translationTable = GetTranslationTable();
+            var input = "{" + inputKeyword + "}";
+            var expected = "{" + outputKeyword + "}";
+            var output = ProcessDelegateVariable.ReplaceVariable(input, translationTable);
+            Console.WriteLine("input   : " + input);
+            Console.WriteLine("output  : " + output);
+            Console.WriteLine("expected: " + expected);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        public void Test_ParseInvalidVariable(string inputKeyword, string outputKeyword, string left, string right)
+        {
+            var translationTable = GetTranslationTable();
+            var input = "{" + left + inputKeyword + right + "}";
+            var expected = input;
+            var output = ProcessDelegateVariable.ReplaceVariable(input, translationTable);
+            Console.WriteLine("input   : " + input);
+            Console.WriteLine("output  : " + output);
+            Console.WriteLine("expected: " + expected);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        [TestCase("Variable1", "VariableA", "", "")]
+        [TestCase("Variable2", "VariableB", "{", "")]
+        [TestCase("Variable3", "VariableC", "", "}")]
+        public void Test_ParseInvalidVariableNoCurlyBracket(string inputKeyword, string outputKeyword, string left, string right)
+        {
+            var translationTable = GetTranslationTable();
+            var input = left + inputKeyword + right;
+            var expected = input;
+            var output = ProcessDelegateVariable.ReplaceVariable(input, translationTable);
+            Console.WriteLine("input   : " + input);
+            Console.WriteLine("output  : " + output);
+            Console.WriteLine("expected: " + expected);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        [TestCase("Variable1", "VariableA", 1)]
+        [TestCase("Variable2", "VariableB", 2)]
+        [TestCase("Variable3", "VariableC", 3)]
+        [TestCase("Variable4", "VariableD", 4)]
+        public void Test_ParseVariableIndex(string inputKeyword, string outputKeyword, int index)
+        {
+            var translationTable = GetTranslationTable();
+            var indexStr = "(" + index.ToString() + ")";
+            var input = "{" + inputKeyword + indexStr + "}";
+            var expected = "{" + outputKeyword + indexStr + "}";
+            var output = ProcessDelegateVariable.ReplaceVariable(input, translationTable);
+            Console.WriteLine("input   : " + input);
+            Console.WriteLine("output  : " + output);
+            Console.WriteLine("expected: " + expected);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+
+        [TestCase("Variable1", "VariableA", 1, "[", "]")]
+        [TestCase("Variable2", "VariableB", 2, "{", "}")]
+        [TestCase("Variable3", "VariableC", 3, "<", ">")]
+        [TestCase("Variable4", "VariableD", 4, "[", "")]
+        [TestCase("Variable5", "VariableE", 5, "{", "")]
+        [TestCase("Variable6", "VariableF", 6, "(", "")]
+        [TestCase("Variable7", "VariableG", 7, "", "]")]
+        [TestCase("Variable8", "VariableH", 8, "", "}")]
+        [TestCase("Variable9", "VariableI", 9, "", ")")]
+        public void Test_ParseVariableInvalidIndex(string inputKeyword, string outputKeyword, int index, string left, string right)
+        {
+            var translationTable = GetTranslationTable();
+            var indexStr = left + index.ToString() + right;
+            var input = "{" + inputKeyword + indexStr + "}";
+            var expected = input;
+            var output = ProcessDelegateVariable.ReplaceVariable(input, translationTable);
+            Console.WriteLine("input   : " + input);
+            Console.WriteLine("output  : " + output);
+            Console.WriteLine("expected: " + expected);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        /// <summary>
+        /// create dictionary for delegate
+        /// </summary>
+        /// <returns></returns>
+        internal Dictionary<string, ProcessDelegateVariable.GetNewText> GetTranslationTable()
+        {
+            var translationTable = new Dictionary<string, ProcessDelegateVariable.GetNewText>();
+            foreach(string keyword in keywordMap.Keys)
+            {
+                translationTable[keyword] = delegateGetNewText;
+            }
+            return translationTable;
+        }
+
+        /// <summary>
+        /// delegate to substitute keywords
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        internal string delegateGetNewText(string keyword, int index)
+        {
+            string newkeyword = string.Empty;
+            if (keywordMap.ContainsKey(keyword))
+            {
+                newkeyword = keywordMap[keyword];
+            }
+
+            if (index < 0)
+            {
+                return newkeyword;
+            }
+            else
+            {
+                return newkeyword + "(" + index.ToString() + ")";
+            }
+        }
+    }
+}
